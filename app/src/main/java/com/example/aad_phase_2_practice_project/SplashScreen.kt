4362e575435a49f62ec55bc031e931de3ca712fc
Utibe <etim.utibe@gmail.com>
2020-08-29 18:39:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aad_phase_2_practice_project/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.aad_phase_2_practice_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
